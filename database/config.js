3701f41b0a1bac61492448de4f5898f8f5826649
Yeison <enmanuelquiterios@gmail.com>
2022-03-24 11:44:37 -0400

    Connection created

--- database/config.js
+++ database/config.js
@@ -5,16 +5,22 @@ const dbConnection = async () => {
     await mongoose.connect(process.env.MONGODB_CNN, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
     });
     console.log("data base online");
   } catch (error) {
     console.log(error);
-    throw new Error("Error when Initiatinz database");
+    throw new Error("Error when Initializing database");
   }
 };
 
 module.exports = {
   dbConnection,
 };
+
+```
+I need to check why they are not supported not more
+
+useCreateIndex: true,
+useFindAndModify: false
+
+```;

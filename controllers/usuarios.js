8ce4efaa438b575f4348a4f3fbe65653521ae13e
Yeison <enmanuelquiterios@gmail.com>
2022-03-24 12:30:28 -0400

    Reviewing steps

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,9 @@
 const { response, request } = require("express");
 const User = require("../models/User");
+
+//
 const usuariosGet = (req = request, res = response) => {
   const { q, nombre = "No name", apikey, page = 1, limit } = req.query;
-
   res.json({
     msg: "get API - controlador",
     q,
@@ -12,6 +13,8 @@ const usuariosGet = (req = request, res = response) => {
     limit,
   });
 };
+
+//
 
 const usuariosPost = async (req, res = response) => {
   const body = req.body;
@@ -25,6 +28,7 @@ const usuariosPost = async (req, res = response) => {
   });
 };
 
+//
 const usuariosPut = (req, res = response) => {
   const { id } = req.params;
 
@@ -34,18 +38,21 @@ const usuariosPut = (req, res = response) => {
   });
 };
 
+//
 const usuariosPatch = (req, res = response) => {
   res.json({
     msg: "patch API - usuariosPatch",
   });
 };
 
+//
 const usuariosDelete = (req, res = response) => {
   res.json({
     msg: "delete API - usuariosDelete",
   });
 };
 
+//
 module.exports = {
   usuariosGet,
   usuariosPost,

2cf6a9a4e73ba9c07b2f04e526949bb092456a33
Yeison <enmanuelquiterios@gmail.com>
2022-03-24 10:25:32 -0400

    Updates

--- models/server.js
+++ models/server.js
@@ -1,46 +1,40 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
 
-    constructor() {
-        this.app  = express();
-        this.port = process.env.PORT;
-        this.usuariosPath = '/api/usuarios';
+    // Middlewares
+    this.middlewares();
 
-        // Middlewares
-        this.middlewares();
+    // Rutas de mi aplicación
+    this.routes();
+  }
 
-        // Rutas de mi aplicación
-        this.routes();
-    }
+  middlewares() {
+    // CORS
+    this.app.use(cors());
 
-    middlewares() {
+    // Lectura y parseo del body
+    // Me permite leer cuando recivo mi request
+    this.app.use(express.json());
 
-        // CORS
-        this.app.use( cors() );
+    // Directorio Público
+    this.app.use(express.static("public"));
+  }
 
-        // Lectura y parseo del body
-        this.app.use( express.json() );
+  routes() {
+    this.app.use(this.usuariosPath, require("../routes/usuarios"));
+  }
 
-        // Directorio Público
-        this.app.use( express.static('public') );
-
-    }
-
-    routes() {
-        this.app.use( this.usuariosPath, require('../routes/usuarios'));
-    }
-
-    listen() {
-        this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port );
-        });
-    }
-
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
 }
-
-
-
 
 module.exports = Server;

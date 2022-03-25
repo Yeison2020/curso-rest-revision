362645604cf40c4265dd60d3571ca22d6fca3a71
Yeison <enmanuelquiterios@gmail.com>
2022-03-25 09:56:12 -0400

    Adding middlewares

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,27 +1,28 @@
+const { Router } = require("express");
 
-const { Router } = require('express');
+// This middleware is comming from express-validator
+const { check } = require("express-validator");
 
-const { usuariosGet,
-        usuariosPut,
-        usuariosPost,
-        usuariosDelete,
-        usuariosPatch } = require('../controllers/usuarios');
+const {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
+} = require("../controllers/usuarios");
 
 const router = Router();
 
+router.get("/", usuariosGet);
 
-router.get('/', usuariosGet );
+router.put("/:id", usuariosPut);
 
-router.put('/:id', usuariosPut );
+// The router takes three arguments 1. path default 2. middleware 3. function Controller
 
-router.post('/', usuariosPost );
+router.post("/", check, usuariosPost);
 
-router.delete('/', usuariosDelete );
+router.delete("/", usuariosDelete);
 
-router.patch('/', usuariosPatch );
-
-
-
-
+router.patch("/", usuariosPatch);
 
 module.exports = router;

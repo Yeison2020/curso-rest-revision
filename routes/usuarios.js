827e08b52b6fc38e5022746b4218b19f6b1ab1c6
Yeison <enmanuelquiterios@gmail.com>
2022-03-25 12:20:51 -0400

    Role validations is working

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const { validarCampos } = require("../middlewares/validar-campos");
 
+const Role = require("../models/role");
+
 // This middleware is comming from express-validator
 const { check } = require("express-validator");
 
@@ -29,6 +31,12 @@ router.post(
     check("name", "Your Email format incorrect").not().isEmpty(),
     check("email", "Your name is requiered").isEmail(),
     check("password", "Should contain more 6 letters").isLength({ min: 6 }),
+    check("role").custom(async (role = "") => {
+      const existeRole = await Role.findOne({ role });
+      if (!existeRole) {
+        throw new Error(`Your ${role} is not in the DB`);
+      }
+    }),
     // check("role", "Not a valid role").isIn(["ADMIN_ROLE", "USER_ROLE"]),
     validarCampos,
   ],

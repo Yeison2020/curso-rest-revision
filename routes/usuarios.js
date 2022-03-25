e7f023ef4d3c34cc3c8504acd683ab4d6f972735
Yeison <enmanuelquiterios@gmail.com>
2022-03-25 12:34:22 -0400

    Added helpers to check for roles

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -37,7 +37,7 @@ router.post(
     check("name", "Your Email format incorrect").not().isEmpty(),
     check("email", "Your name is requiered").isEmail(),
     check("password", "Should contain more 6 letters").isLength({ min: 6 }),
-    check("role").custom(isRoleValid()),
+    check("role").custom(isRoleValid),
     // check("role", "Not a valid role").isIn(["ADMIN_ROLE", "USER_ROLE"]),
     validarCampos,
   ],
